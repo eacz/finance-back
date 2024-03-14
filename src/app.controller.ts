647d8028ae6a81f0d70771a09feb76c7b3203835
Esteban Canteros <estebanacanteros@gmail.com>
2024-03-14 19:22:49 -0300

    remove xd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Auth } from './auth/decorators/auth.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @Auth()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
